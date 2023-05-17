2783ed5e80f664349ad7fb297c12e22e3c33682e
billy rennekamp <billy.rennekamp@gmail.com>
2023-05-17 21:19:33 +0900

    return it how it came in

--- src/functions/color-helpers.js
+++ src/functions/color-helpers.js
@@ -1,13 +1,13 @@
 module.exports = {
-    hexToRgb: (hex) => {
-        if (typeof hex !== 'string') return hex
+  hexToRgb: (hex) => {
+    if (typeof hex !== 'string') return hex
 
-        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
-        hex = hex.replace(shorthandRegex, (m, r, g, b) => {
-            return r + r + g + g + b + b
-        })
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
+    hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+      return r + r + g + g + b + b
+    })
 
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-        return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null
-    }
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+    return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null
+  }
 }
